04be5d4155c6ddbca83c0ee89ed27941ba8b24f7
luke-ashford <mail@luke-ashford.com>
2026-04-13 02:57:29 +0100

    feat: made "prod" ready by finalising docker file and fixing flask run arguments

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ COPY . /sos-app
 
 WORKDIR /sos-app
 
+RUN mkdir -p /uploads
 RUN pip install -r requirements.txt
 
 EXPOSE 5000
--- app.py
+++ app.py
@@ -28,6 +28,6 @@ def audio():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
 
 # TODO: Setup Kafka
