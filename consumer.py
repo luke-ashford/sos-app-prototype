d1c3928554186bf6f9387cab8b7af0dceca8e160
luke-ashford <mail@luke-ashford.com>
2026-04-18 00:05:28 +0100

    feat: added logging to consumer.py fix: added decoding of message value back to bytes

--- consumer.py
+++ consumer.py
@@ -1,5 +1,6 @@
 from kafka import KafkaConsumer
 from random import randint
+from base64 import b64decode
 
 counter = randint(1, 1000000)
 
@@ -11,5 +12,6 @@ consumer = KafkaConsumer(
 
 for msg in consumer:
     with open(f"uploads/recording_{counter}.webm", "wb") as f:
-        f.write(msg)
+        f.write(b64decode(msg.value))
         counter += 1
+        print(f"Written uploads/recording_{counter}.webm")
